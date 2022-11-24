c5784d0e33a905803d4afc910bd9dd955181dd3b
kaylarubin <kayla.rose12@yahoo.com>
2022-11-23 21:00:49 -0700

    added back in threat ring logic and removed unused imports

--- src/tsx/MapWrapper.tsx
+++ src/tsx/MapWrapper.tsx
@@ -8,52 +8,46 @@ import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { OSM } from "ol/source";
 import Feature from "ol/Feature";
-import { Geometry, Point } from "ol/geom";
+import { Geometry } from "ol/geom";
 import GeoJSON from "ol/format/GeoJSON";
 import { default as TurfCircle } from "@turf/circle";
 import union from "@turf/union";
-import { transform } from "ol/proj";
-import { Fill, RegularShape, Stroke, Style } from "ol/style";
-import { Coordinate } from "ol/coordinate";
+import { Fill, Stroke, Style } from "ol/style";
 import { TriangleButton } from "./TriangleButton";
 import { transformToOpenLayerProjection } from "../supportFunctions";
-import WMTS from "ol/source/WMTS";
-import WMTSTileGrid from "ol/tilegrid/WMTS";
-import { get as getProjection } from "ol/proj";
-import { getTopLeft, getWidth } from "ol/extent";
 import { mapLayerContext } from "../context/MapLayerConext";
-import { OwnshipLayer } from "./MapLayers/OwnshipLayer";
 import { TriangleLayer } from "./MapLayers/TriangleLayer";
 
-// const circleStyle = new Style({
-//   stroke: new Stroke({
-//     color: "red",
-//     width: 3,
-//   }),
-//   fill: new Fill({
-//     color: "rgba(0, 0, 255, 0.1)",
-//   }),
-// });
+//Interfaces
+interface ThreatRing {
+  lat: number;
+  lon: number;
+  radius: number;
+}
 
-// const threatRings: ThreatRing[] = [
-//   { lat: 36.2322, lon: -115.065, radius: 40 },
-//   { lat: 36.2322, lon: -115.865, radius: 40 },
-//   { lat: 36.2322, lon: -114, radius: 40 },
-// ];
-
-// const ringCenter = [-115.065, 36.2322];
-// const slcCenter = [-111.9905247, 40.7767168];
-const nellis = [-115.065, 36.2322];
-
-// interface ThreatRing {
-//   lat: number;
-//   lon: number;
-//   radius: number;
-// }
-
+//Enums
 enum MapLayer {
   TRIANGLE_LAYER,
 }
+
+//Constants
+const circleStyle = new Style({
+  stroke: new Stroke({
+    color: "red",
+    width: 3,
+  }),
+  fill: new Fill({
+    color: "rgba(0, 0, 255, 0.1)",
+  }),
+});
+
+const threatRings: ThreatRing[] = [
+  { lat: 36.2322, lon: -115.065, radius: 40 },
+  { lat: 36.2322, lon: -115.865, radius: 40 },
+  { lat: 36.2322, lon: -114, radius: 40 },
+];
+
+const nellis = [-115.065, 36.2322];
 
 const MapWrapper: React.FC = () => {
   // get ref to div element - OpenLayers will render into this div
@@ -62,31 +56,31 @@ const MapWrapper: React.FC = () => {
   const mapLayerApi = useContext(mapLayerContext);
 
   useEffect(() => {
-    // //Create array of turf circles
-    // const turfCircles = new Array<any>();
-    // threatRings.forEach((threatRing: ThreatRing) => {
-    //   const center = [threatRing.lon, threatRing.lat];
-    //   const turfCircle = TurfCircle(center, threatRing.radius);
-    //   turfCircles.push(turfCircle);
-    // });
+    //Create array of turf circles
+    const turfCircles = new Array<any>();
+    threatRings.forEach((threatRing: ThreatRing) => {
+      const center = [threatRing.lon, threatRing.lat];
+      const turfCircle = TurfCircle(center, threatRing.radius);
+      turfCircles.push(turfCircle);
+    });
 
-    // //create single turf feature from all turf circles
-    // let unionFeature: any;
-    // for (var i = 1; i < turfCircles.length; i++) {
-    //   if (i === 1) {
-    //     unionFeature = union(turfCircles[i - 1], turfCircles[i]);
-    //   } else {
-    //     unionFeature = union(unionFeature, turfCircles[i]);
-    //   }
-    // }
+    //create single turf feature from all turf circles
+    let unionFeature: any;
+    for (var i = 1; i < turfCircles.length; i++) {
+      if (i === 1) {
+        unionFeature = union(turfCircles[i - 1], turfCircles[i]);
+      } else {
+        unionFeature = union(unionFeature, turfCircles[i]);
+      }
+    }
 
-    // //convert turf feature to openlayers feature
-    // const olCircleFeature: Feature<Geometry> = new GeoJSON().readFeature(
-    //   unionFeature
-    // );
-    // olCircleFeature.setId("unionFeature");
-    // olCircleFeature.getGeometry()?.transform("EPSG:4326", "EPSG:3857");
-    // olCircleFeature.setStyle(circleStyle);
+    //convert turf feature to openlayers feature
+    const olCircleFeature: Feature<Geometry> = new GeoJSON().readFeature(
+      unionFeature
+    );
+    olCircleFeature.setId("unionFeature");
+    olCircleFeature.getGeometry()?.transform("EPSG:4326", "EPSG:3857");
+    olCircleFeature.setStyle(circleStyle);
 
     const initialMap = new Map({
       target: mapElement.current!,
@@ -95,11 +89,11 @@ const MapWrapper: React.FC = () => {
           source: new OSM(),
           visible: true,
         }),
-        // new VectorLayer({
-        //   source: new VectorSource({
-        //     features: [olCircleFeature],
-        //   }),
-        // }),
+        new VectorLayer({
+          source: new VectorSource({
+            features: [olCircleFeature],
+          }),
+        }),
         mapLayerApi.triangleLayer(),
         mapLayerApi.ownshipLayer(),
       ],
